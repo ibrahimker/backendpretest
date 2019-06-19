a27b721136c7c303fe8ee0c31a982d53bded5e4d
Ibrahim Nurandita Isbandiputra <ibrahim.isbandiputra@tiket.com>
2019-06-20 01:37:36 +0700

    Problem 2 finisihed

--- app.spec.js
+++ app.spec.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('./app');
-describe('Test the root path', () => {
+describe('API should be able to send message', () => {
     it('It should call sendmessage API', (done) => {
         request(app).get('/sendmessage').then((response) => {
             expect(response.statusCode).toBe(200);
@@ -21,4 +21,27 @@ describe('Test the root path', () => {
           done();
       });
     });
+});
+
+describe('API should be able to get outgoing message', () => {
+  it('It should call getmessage API', (done) => {
+      request(app).get('/sendmessage').then((response) => {
+          expect(response.statusCode).toBe(200);
+          done();
+      });
+  });
+
+  it('It should send and received HAI', (done) => {
+    request(app).get('/sendmessage?message=HAI').then((response) => {
+        expect(response.text).toBe('HAI');
+        done();
+    });
+  });
+
+  it('It should get all outgoing response', (done) => {
+    request(app).get('/getmessage').then((response) => {
+        expect(response.text).toBe("[\"No Message Found\",\"HAI\",\"No Message Found\",\"No Message Found\",\"HAI\"]");
+        done();
+    });
+  });
 });
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+// dummy database for storing outgoing message
+let database = [];
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,7 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // send message API
 app.get('/sendmessage', async (req, res) => {
   const message = (req.query.message) ? req.query.message : 'No Message Found';
+  database.push(message);
   res.send(message);
+});
+
+// get outgoing message API
+app.get('/getmessage', (req, res) => {  
+  res.send(database);
 });
 
 module.exports = app;
